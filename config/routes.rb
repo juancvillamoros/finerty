105360dc531d469f930ffe736f13342be946110f
juancviillamoros <jviillamoros@gmail.com>
2023-01-19 20:13:39 -0500

    creando las rutas.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-
+    resources :users do
+      resources :salaries, only: [:index, :create, :update, :destroy]
+      resources :salary_histories, only: [:index, :create, :update, :destroy]
+    end
+    resources :taxes, only: [:index, :create, :update, :destroy]
 end
+  
